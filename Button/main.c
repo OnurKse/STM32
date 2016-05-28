d66cc61f958af71939221232423f657385c1f501
Onur Köse <onuur94@hotmail.com>
2016-05-28 11:33:38 +0300

    Update main.c

--- Button/main.c
+++ Button/main.c
@@ -1,20 +1,17 @@
-#include "delay.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
 
 GPIO_InitTypeDef GPIO_InitStructure;
 
-void SysTick_Handler()
-{
-	TimingDelay_Decrement();
-}
-
-void init_GPIO()
+void init_GPIO(void)
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
@@ -22,30 +19,25 @@ void init_GPIO()
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
-int i;
-int main(){
-	
-	SystemCoreClockUpdate();
-	if (SysTick_Config(SystemCoreClock / 1000)) {
-		while (1);
-	}
+int main(void)
+{
 	init_GPIO();
-	
+
 	while(1)
 	{
-		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0))
+		if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0))
 		{
-			GPIO_SetBits(GPIOD, GPIO_Pin_13);
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
 		}
 		else
 		{
-			GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
 		}
-	 }
+	}
 }
